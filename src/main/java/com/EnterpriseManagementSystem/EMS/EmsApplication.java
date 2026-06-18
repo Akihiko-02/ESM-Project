9b20a65ead16dd5c3644ebd90cd3d09feb212ced
Nanda Shine <thawn819@gmail.com>
2026-06-18 17:37:12 +0630

    This is the EMS program with model and repostory implementation..

--- src/main/java/com/EnterpriseManagementSystem/EMS/EmsApplication.java
+++ src/main/java/com/EnterpriseManagementSystem/EMS/EmsApplication.java
@@ -8,7 +8,7 @@ public class EmsApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(EmsApplication.class, args);gg
+		SpringApplication.run(EmsApplication.class, args);
 		System.out.println("EMS Application Started");
 	}
 
